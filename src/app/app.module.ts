b182bb6dd45d0c6f0257248790f39d041f0a799f
Alvaro Basaurit Marin <alvaro.basaurit@kerabenet.local>
2021-03-17 10:31:17 +0100

    initial tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//-----------------------------------------
+import { HolaMundo } from './componentes/holamundo/holamundo.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HolaMundo
   ],
   imports: [
     BrowserModule,
